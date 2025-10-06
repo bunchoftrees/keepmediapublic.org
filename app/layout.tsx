62bb3068ab4345c9601123fcaf5a4149ccb51dfd
John Hartley <john@johnbhart.com>
2025-10-05 22:35:32 -0500

    adds analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import GlobalBanner from "./components/GlobalBanner";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         <GlobalBanner />
         {children}
+        <Analytics />
       </body>
     </html>
   );
